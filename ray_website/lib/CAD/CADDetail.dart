e7a4ce45ad0afe531f78d19bf98caff977a95c36
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-08-18 14:55:34 -0400

    a

--- ray_website/lib/CAD/CADDetail.dart
+++ ray_website/lib/CAD/CADDetail.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ray_website/fancyButton.dart';
 
 class cadMode extends StatefulWidget {
   cadMode(
@@ -36,105 +37,99 @@ class _cadModeState extends State<cadMode> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          width: _screenWidth / 1.4,
-          height: _screenH * 1.1,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  colorFilter: ColorFilter.mode(
-                      Colors.white.withOpacity(0.9), BlendMode.dstATop),
-                  image: AssetImage(widget.showImg!),
-                  fit: BoxFit.fitWidth)),
-          child: Row(
-            children: [
-              SizedBox(
-                width: _screenWidth / 1.35,
+      body: Row(
+        children: [
+          Container(
+            width: _screenWidth / 1.4,
+            height: _screenH * 1.1,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.white.withOpacity(0.9), BlendMode.dstATop),
+                    image: AssetImage(widget.showImg!),
+                    fit: BoxFit.fitWidth)),
+          ),
+          Column(
+            children: AnimationConfiguration.toStaggeredList(
+              duration: const Duration(milliseconds: 705),
+              childAnimationBuilder: (widget) => SlideAnimation(
+                child: FadeInAnimation(
+                  child: widget,
+                ),
+                verticalOffset: 220,
               ),
-              Column(
-                children: AnimationConfiguration.toStaggeredList(
-                  duration: const Duration(milliseconds: 705),
-                  childAnimationBuilder: (widget) => SlideAnimation(
-                    child: FadeInAnimation(
-                      child: widget,
-                    ),
-                    verticalOffset: 220,
+              children: [
+                SizedBox(
+                  height: _screenH / 20,
+                ),
+                Container(
+                  width: _screenWidth / 3.5,
+                  padding: EdgeInsets.fromLTRB(
+                    _screenWidth / 40,
+                    _screenH / 40,
+                    _screenWidth / 40,
+                    _screenH / 40,
                   ),
-                  children: [
+                  child: SingleChildScrollView(
+                      child: Column(children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                    color: Colors.grey.shade100, width: 5))),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Created on:\n" + widget.date!,
+                              style: TextStyle(
+                                fontSize: _screenH / 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey.shade500,
+                                fontFamily: "coms",
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              width: _screenWidth / 32,
+                            ),
+                            Icon(
+                              FontAwesomeIcons.trophy,
+                              size: _screenH / 18,
+                            )
+                          ],
+                        )),
                     SizedBox(
-                      height: _screenH / 2.3,
+                      height: _screenH / 15,
+                    ),
+                    Text(
+                      widget.title!,
+                      style: TextStyle(
+                        fontSize: _screenH / 14,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "yuan",
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: _screenH / 40,
                     ),
                     Container(
-                      height: _screenH / 2,
-                      width: _screenWidth / 4,
-                      padding: EdgeInsets.fromLTRB(
-                        _screenWidth / 33,
-                        _screenH / 40,
-                        _screenWidth / 33,
-                        _screenH / 40,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        color: Colors.grey.shade50.withOpacity(0.8),
-                        gradient: new LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [
-                              Colors.grey.shade200,
-                              Colors.grey.shade50,
-                            ]),
-                      ),
-                      child: SingleChildScrollView(
-                          child: Column(children: [
-                        Container(
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        color: Colors.grey.shade100,
-                                        width: 5))),
-                            child: Row(
-                              children: [
-                                Text(
-                                  "Created on:\n" + widget.date!,
-                                  style: TextStyle(
-                                    fontSize: _screenH / 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.grey.shade500,
-                                    fontFamily: "coms",
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),
-                                Icon(
-                                  FontAwesomeIcons.trophy,
-                                  size: _screenH / 18,
-                                )
-                              ],
-                            )),
-                        Text(
-                          widget.title!,
-                          style: TextStyle(
-                            fontSize: _screenH / 16,
+                      child: Text(
+                        widget.content!,
+                        style: TextStyle(
+                            fontSize: _screenH / 25,
                             fontWeight: FontWeight.bold,
-                            fontFamily: "yuan",
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Container(
-                          child: Text(
-                            widget.content!,
-                            style: TextStyle(
-                                fontSize: _screenH / 30,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: "sw"),
-                            textAlign: TextAlign.center,
-                          ),
-                        )
-                      ])),
-                    )
-                  ],
-                ),
-              )
-            ],
-          )),
+                            fontFamily: "sw"),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ])),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ ray_website/lib/fancyButton.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+class fancyBut extends StatefulWidget {
+  final Widget? pageTo;
+  final IconData? icon;
+  final double? width;
+  final double? height;
+  final String? buttonName;
+  final double? fontsize;
+  fancyBut(
+      {@required this.pageTo,
+      @required this.width,
+      @required this.height,
+      @required this.icon,
+      @required this.buttonName,
+      @optionalTypeArgs this.fontsize,
+      Key? key})
+      : super(key: key);
+
+  @override
+  _fancyButState createState() => _fancyButState();
+}
+
+class _fancyButState extends State<fancyBut> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints:
+          BoxConstraints.expand(width: widget.width, height: widget.height),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(widget.height! / 2),
+          border: Border.all(color: Colors.grey.shade300, width: 5)),
+      child: FlatButton.icon(
+          splashColor: Colors.redAccent[100],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(widget.height! / 2),
+          ),
+          color: Colors.grey.shade600.withOpacity(0.8),
+          onPressed: () {
+            Navigator.push(
+                context,
+                PageTransition(
+                    duration: const Duration(milliseconds: 500),
+                    child: widget.pageTo!,
+                    type: PageTransitionType.rightToLeft));
+          },
+          icon: Icon(
+            widget.icon,
+            color: Colors.white,
+          ),
+          label: Text(
+            widget.buttonName!,
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                fontSize: widget.fontsize ?? 14),
+          )),
+    );
+  }
+}
