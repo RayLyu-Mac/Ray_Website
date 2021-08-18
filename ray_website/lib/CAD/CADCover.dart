16f928f24d6a72cb3edad539294431818e2ca541
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-08-18 13:52:53 -0400

    a

--- ray_website/lib/CAD/CADCover.dart
+++ ray_website/lib/CAD/CADCover.dart
@@ -18,6 +18,22 @@ class _CADCoverState extends State<CADCover> {
     "assest/3Dprint/cover/4.png",
     "assest/3Dprint/cover/5.png",
   ];
+  List<List> comment = [
+    ["Fun", "Create your own toys, reviewing the fun back childhood"],
+    [
+      "Creation",
+      "Let your creative mind fly, design the coolest stuff in the world!"
+    ],
+    [
+      "Invention",
+      "Forget the stuff has been created, you can invent the new stuff that haven't been existed"
+    ],
+    ["Posses", "See the cool design but can't afford? Use CAD and print it!"],
+    [
+      "Practice",
+      "Train your skills on technical design and prepare yourself for the challenge tomorrow!"
+    ]
+  ];
 
   @override
   void didChangeDependencies() {
@@ -31,6 +47,7 @@ class _CADCoverState extends State<CADCover> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ImageSlideshow(
+          isLoop: true,
           width: double.infinity,
           height: _screenH,
           indicatorColor: Colors.blue,
@@ -39,10 +56,35 @@ class _CADCoverState extends State<CADCover> {
           children: [
             for (var j = 0; j < coverImgs.length; j++)
               Container(
+                child: Center(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: _screenH / 15,
+                      ),
+                      Text("CAD is " + comment[j][0],
+                          style: TextStyle(
+                            fontFamily: "coms",
+                            fontSize: _screenH / 15,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      SizedBox(
+                        height: _screenH / 25,
+                      ),
+                      Text(comment[j][1],
+                          style: TextStyle(
+                            fontFamily: "coms",
+                            fontSize: _screenH / 20,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
+                  ),
+                ),
                 decoration: BoxDecoration(
                     image: DecorationImage(
                         colorFilter: ColorFilter.mode(
-                            Colors.white.withOpacity(0.9), BlendMode.dstATop),
+                            Colors.blueGrey.withOpacity(0.5),
+                            BlendMode.dstATop),
                         image: AssetImage(coverImgs[j]),
                         fit: BoxFit.fitWidth)),
               )
