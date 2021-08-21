6360d154cba45dc02d90ff45eae58f78cb35c2b6
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-08-20 21:09:12 -0400

    a

---
+++ ray_website/lib/Album/natureAlbum/JHE/JHEMain.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:ray_website/Album/natureAlbum/fourSeasonMode.dart';
+
+class JHEMain extends StatefulWidget {
+  JHEMain({Key? key}) : super(key: key);
+
+  @override
+  _JHEMainState createState() => _JHEMainState();
+}
+
+class _JHEMainState extends State<JHEMain> {
+  List<List> cont = [
+    ["assest/nature/jhe/s1.png", "Spring"],
+    ["assest/nature/jhe/s2.png", "Summer"],
+    ["assest/nature/jhe/s3.png", "Fall"],
+    ["assest/nature/jhe/s4.png", "Winter"],
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return fourSeason(
+        backImg: "assest/back3.png",
+        content:
+            "John Hodgins Engineering Building, The home for McMaster Engineer",
+        pics: cont,
+        title: "JHE Four Seasons");
+  }
+}
