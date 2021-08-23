8257a3a631baffbb6e2cf8621233582aaca603c8
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-08-22 20:15:12 -0400

    a

--- ray_website/lib/Poem/PoemMain.dart
+++ ray_website/lib/Poem/PoemMain.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:ray_website/BlackFilmUni.dart';
+import 'package:ray_website/CurtainLeft.dart';
 import 'package:ray_website/backgroundPic.dart';
 import 'package:ray_website/Teacher/style.dart';
 import 'SanWen.dart';
+import 'package:ray_website/curtainRight.dart';
 
 class PoemMain extends StatefulWidget {
   PoemMain({Key? key}) : super(key: key);
@@ -29,46 +30,57 @@ class _PoemMainState extends State<PoemMain> {
       appBar: AppBar(
         title: title(content: "As a Poet", fontSize: _screenH / 12),
         centerTitle: true,
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.pinkAccent.shade100,
       ),
-      body: backGroundPic(
-          child: Column(
-            children: [
-              SizedBox(
-                height: _screenH / 12,
-              ),
-              title(content: "Poem is a life attitude", fontSize: _screenH / 7),
-              content(
-                contents: "         《南曲北调》-霜樱居士",
-                fontSize: _screenH / 18,
-                fontf: "yuan",
-              ),
-              SizedBox(
-                height: _screenH / 20,
-              ),
-              Row(
-                children: [
-                  BlackFilm(
-                    showPic: "assest/background/back7.png",
-                    content: "又称现代诗，没有固定的诗体，那就追随着作者的诗情，感受其魅力",
-                    title: "散文诗",
-                    fontC: Colors.limeAccent,
-                    pageTo: SanWen(),
-                    fontf: "yuan",
-                  ),
-                  BlackFilm(
-                    showPic: "assest/background/back7.png",
-                    content: "又称现代诗，没有固定的诗体，那就追随着作者的诗情，感受其魅力",
-                    title: "古体诗",
-                    fontC: Colors.limeAccent,
-                    pageTo: SanWen(),
-                    fontf: "yuan",
-                  )
-                ],
-              )
-            ],
-          ),
-          pic: "assest/background/back5.png"),
+      body: SingleChildScrollView(
+        child: backGroundPic(
+            transp: 0.4,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: _screenH / 20,
+                ),
+                title(
+                    content: "Poem is a life attitude",
+                    fontSize: _screenH / 10),
+                content(
+                  contents:
+                      "                                                                   《南曲北调》-霜樱居士",
+                  fontSize: _screenH / 18,
+                  fontf: "yuan",
+                ),
+                SizedBox(
+                  height: _screenH / 5,
+                ),
+                Row(
+                  children: [
+                    CurtainLeft(
+                      showPic: "assest/background/back7.png",
+                      content:
+                          "又称现代诗，没有固定的诗体，兼有诗与散文特点的一种现代抒情文学体裁。一般基于社会和人生背景的感触，体裁丰富。在这部诗集里收录了作者5篇散文诗（百度百科）",
+                      title: "散文诗",
+                      fontC: Colors.limeAccent,
+                      pageTo: SanWen(),
+                      width: _screenWidth / 2.1,
+                      height: _screenH / 1.55,
+                      fontf: "yuan",
+                    ),
+                    CurtainRight(
+                      showPic: "assest/background/back8.png",
+                      content: "根据诗句的字数，分为四言，五言，七言不等。这篇诗集中收录了作者35篇古体诗。",
+                      title: "古体诗",
+                      fontC: Colors.blueAccent,
+                      pageTo: SanWen(),
+                      width: _screenWidth / 2.1,
+                      height: _screenH / 1.55,
+                      fontf: "yuan",
+                    )
+                  ],
+                )
+              ],
+            ),
+            pic: "assest/background/back5.png"),
+      ),
     );
   }
 }
