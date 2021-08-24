b8cc35d7b3c15985daf7e6f156c48aa63864d3dc
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-08-24 11:18:33 -0400

    Create Shui.dart

---
+++ ray_website/lib/Poem/GuTiZhong/Shui.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:ray_website/Poem/PoemMode.dart';
+
+class Shui extends StatefulWidget {
+  Shui({Key? key}) : super(key: key);
+
+  @override
+  _ShuiState createState() => _ShuiState();
+}
+
+class _ShuiState extends State<Shui> {
+  List<List> contents = [
+    [
+      "assest/poem/guTi/s1.png",
+      "临瀑",
+      "森林的深处，有一孤泉在默默流淌",
+      "早晨，昨日的露水还未完全消逝。寻访于幽林之中，似闻远处泉水潺潺。快步至山间瀑布之缘，仰观之高出飞泻的水花，拍击在青山之上，落于深潭之中。仿若闻不绝琴声，经久不息",
+      "2020-08-15"
+    ],
+    [
+      "assest/poem/guTi/s1.png",
+      "倚礁",
+      "靠着湖边的礁石，远望湖上波涛翻滚",
+      "湖风，海鸥，白帆，依靠在礁石边的的他尽收眼底。看着湖鹅悠闲的随波逐流，让他变得平静，迎面吹来的咸咸湿湿的湖风，让他变得疲惫。好像多享受一会",
+      "2020-06-22",
+    ],
+    [""]
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
