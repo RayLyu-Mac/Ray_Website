0a6f50834d9b3dc6d5c310927a48cb21c2c88610
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-08-25 13:45:02 -0400

    a

--- ray_website/lib/PhoneAppD/PhoneMain.dart
+++ ray_website/lib/PhoneAppD/PhoneMain.dart
@@ -9,11 +9,29 @@ class PhoneAppDeveloper extends StatefulWidget {
 }
 
 class _PhoneAppDeveloperState extends State<PhoneAppDeveloper> {
+  bool _hovering = false;
+  void _mouseEnter(bool hover) {
+    setState(() {
+      _hovering = hover;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("As a phone developer"),
+      ),
+      body: backGroundPic(
+        child: Column(
+          children: [
+            Row(
+              children: [],
+            )
+          ],
+        ),
+        pic: "assest/background/back17.png",
+        transp: 0.3,
       ),
     );
   }
