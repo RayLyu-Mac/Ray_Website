6cfacb44d8c77505040d6dce708e9d5ef800c47b
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-08-28 11:36:03 -0400

    Update AppTools.dart

--- ray_website/lib/PhoneAppD/Feature/AppTools.dart
+++ ray_website/lib/PhoneAppD/Feature/AppTools.dart
@@ -73,59 +73,62 @@ class _AppToolState extends State<AppTool> {
       appBar: AppBar(
         title: Text("App Tools"),
       ),
-      body: backGroundPic(
-          transp: 0.45,
-          child: Column(
-            children: [
-              SizedBox(
-                height: _screenH / 15,
-              ),
-              Text(
-                "Matls Eng Tool Box",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: _screenH / 12,
-                    fontFamily: "bank"),
-              ),
-              SizedBox(
-                height: _screenH / 20,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: _screenH / 1.6,
-                    width: _screenWidth / 4.2,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage("assest/App/t1.png"),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.circular(20),
-                        border:
-                            Border.all(width: 7, color: Colors.grey.shade300)),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      for (var i = 0; i < con.length; i++)
-                        Button(
-                            buttonIcon: con[i][3],
-                            titleColor: con[i][4],
-                            pageTo: cadMode(
-                                fit: true,
-                                content: con[i][2],
-                                date: "2021 Summer",
-                                showImg: con[i][0],
-                                title: con[i][1]),
-                            title: con[i][1])
-                    ],
-                  )
-                ],
-              )
-            ],
-          ),
-          pic: "assest/background/back31.png"),
+      body: SingleChildScrollView(
+        child: backGroundPic(
+            transp: 0.45,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: _screenH / 15,
+                ),
+                Text(
+                  "Matls Eng Tool Box",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: _screenH / 12,
+                      fontFamily: "bank"),
+                ),
+                SizedBox(
+                  height: _screenH / 20,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: _screenH / 1.6,
+                      width: _screenWidth / 4.2,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assest/App/t1.png"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                              width: 7, color: Colors.grey.shade300)),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        for (var i = 0; i < con.length; i++)
+                          Button(
+                              buttonIcon: con[i][3],
+                              titleColor: con[i][4],
+                              pageTo: cadMode(
+                                  fit: true,
+                                  content: con[i][2],
+                                  date: "2021 Summer",
+                                  showImg: con[i][0],
+                                  title: con[i][1]),
+                              title: con[i][1])
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+            pic: "assest/background/back31.png"),
+      ),
     );
   }
 }
