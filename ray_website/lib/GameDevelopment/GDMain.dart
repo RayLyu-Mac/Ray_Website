4e136fdaeebe0e8fdef3239f3ef43b724d3c3dba
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2021-09-06 14:46:52 -0400

    Update GDMain.dart

--- ray_website/lib/GameDevelopment/GDMain.dart
+++ ray_website/lib/GameDevelopment/GDMain.dart
@@ -43,7 +43,54 @@ class _GameDesignState extends State<GameDesign> {
                 fontFamily: "sw",
                 fontSize: _screenH / 8,
                 fontWeight: FontWeight.bold,
-              ))
+              )),
+          Row(
+            children: [
+              Container(
+                width: _screenWidth / 3.5,
+                height: _screenH / 5,
+                child: ImageSlideshow(children: [
+                  for (var i = 0; i < pics.length; i++)
+                    Image(image: AssetImage(pics[i]))
+                ]),
+              ),
+              Container(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text("Purpose",
+                          style: TextStyle(
+                            color: Colors.grey.shade800,
+                            fontFamily: "bank",
+                            fontSize: _screenH / 13,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      // https://www.youtube.com/watch?v=84njPYepKIU&t=7s
+                      SizedBox(
+                        height: _screenH / 25,
+                      ),
+                      Text(
+                          "The purpose of this simulation including following aspect: By creating a collision model to learn how COVID is spread in a closed region. Learning how a people behavior will affect the virus spread by introducing different avatar and giving them different behavior properties. Finally, by learning how different government policy affect the socio-econmoic status"),
+                      SizedBox(
+                        height: _screenH / 15,
+                      ),
+                      Text("Reference",
+                          style: TextStyle(
+                            color: Colors.grey.shade800,
+                            fontFamily: "bank",
+                            fontSize: _screenH / 13,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      // https://www.youtube.com/watch?v=84njPYepKIU&t=7s
+                      SizedBox(
+                        height: _screenH / 25,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
