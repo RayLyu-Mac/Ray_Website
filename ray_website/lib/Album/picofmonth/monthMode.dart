5b451892e1f7554323d4635bb4578e63fcb0d1d3
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2022-04-30 16:44:25 -0400

    a

---
+++ ray_website/lib/Album/picofmonth/monthMode.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class pmMode extends StatefulWidget {
+  pmMode({Key? key}) : super(key: key);
+
+  @override
+  State<pmMode> createState() => _pmModeState();
+}
+
+class _pmModeState extends State<pmMode> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      body: Stack(children: []),
+    );
+  }
+}
