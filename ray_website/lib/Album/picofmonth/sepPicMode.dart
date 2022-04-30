640965d4ec9209d83c7b6ed08726307115bb9bf7
RayLyu <59774755+RayLyu-Mac@users.noreply.github.com>
2022-04-30 16:51:39 -0400

    a

--- ray_website/lib/Album/picofmonth/sepPicMode.dart
+++ ray_website/lib/Album/picofmonth/sepPicMode.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class sepPicMode extends StatefulWidget {
+  String? decoImg;
+  sepPicMode({@required this.decoImg, Key? key}) : super(key: key);
+
+  @override
+  State<sepPicMode> createState() => _sepPicModeState();
+}
+
+class _sepPicModeState extends State<sepPicMode> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(image: NetworkImage(widget.decoImg!)),
+          border: Border.all(color: Colors.white, width: 5),
+          borderRadius: BorderRadius.circular(5)),
+    );
+  }
+}
